d27f9270a5d98c192e4549916c644f8999486a3a
priyanshu <priyanshu.singh@vayuz.com>
2025-10-15 14:08:47 +0530

    changes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./src/config/db.js";
+import authRoutes from "./src/routes/authRoutes.js";
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 dotenv.config();
 connectDB();
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
