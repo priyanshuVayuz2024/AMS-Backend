26912f682102b1f0818f2cf8de39384d682af098
priyanshu <priyanshu.singh@vayuz.com>
2025-10-15 15:56:05 +0530

    change

--- src/middlewares/validateRequestData.js
+++ src/middlewares/validateRequestData.js
@@ -1,10 +1,14 @@
+import { sendErrorResponse } from "../util/responseHandler";
+
 export const validateRequestData = (schema) => {
   return (req, res, next) => {
     const { error, value } = schema.validate(req.body, { abortEarly: false });
     if (error) {
-      return res.status(400).json({
+      return sendErrorResponse({
+        res,
+        statusCode: 400,
         message: "Validation error",
-        errors: error.details.map((err) => err.message),
+        error: error.details.map((err) => err.message),
       });
     }
     req.body = value; // sanitized input
